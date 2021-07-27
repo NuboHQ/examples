7d352ae49ad19cb4e833f249b9d9bb6723f34307
nubodev <developer@nubo.app>
2021-07-27 15:39:50 +0100

    Added Fastify example

---
+++ fastify/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const LOGGER = process.env.LOGGER === 'true';
+const PORT = process.env.PORT || 3000;
+const REGION = process.env.NUBO_REGION || 'unknown';
+const LOCATION = process.env.NUBO_LOCATION || 'unknown';
+const fastify = require('fastify')({ logger: LOGGER });
+const path = require('path');
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+});
+
+fastify.register(require('point-of-view'), {
+  engine: {
+    ejs: require('handlebars'),
+  },
+});
+
+fastify.get('*', async (request, reply) => {
+  return reply.view('/templates/index.hbs', {
+    region: REGION,
+    location: LOCATION,
+  });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+    console.log(`> http://localhost:${PORT}`);
+    console.log(`> region: ${REGION}`);
+    console.log(`> location: ${LOCATION}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
