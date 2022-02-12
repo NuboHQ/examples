dd0887e7d97ebfd50ae9542a70500bcc2c3050c6
Marcel Thomas <marcelthomas5@gmail.com>
2022-02-12 12:35:54 +0000

    Update tailwind.config.js

--- node/next/tailwind.config.js
+++ node/next/tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
     extend: {
