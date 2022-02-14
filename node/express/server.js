ec1369c5a7fadccc8836e42e22bce64d330a46dd
Marcel Thomas <marcelthomas5@gmail.com>
2022-02-14 16:02:44 +0000

    test

--- node/express/server.js
+++ node/express/server.js
@@ -4,17 +4,17 @@ const PORT = process.env.PORT || 5555;
 const REGION = process.env.NUBO_REGION || 'unknown';
 const LOCATION = process.env.NUBO_LOCATION || 'unknown';
 
-const VERSION = '3';
+const VERSION = '2';
 
 server.use(helmet());
 
 server.use('*', (req, res) => {
-    res.send(
-        `<div style="font-family: sans-serif; text-align: center; position: fixed; top: 45%; left: 50%; transform: translateX(-50%) translateY(-50%);">
+  res.send(
+    `<div style="font-family: sans-serif; text-align: center; position: fixed; top: 45%; left: 50%; transform: translateX(-50%) translateY(-50%);">
             <h1>Express on Nubo</h1>
             <p style="color: #777">${REGION}<br/><br/>${LOCATION}</p>
-         </div>`,
-    );
+         </div>`
+  );
 });
 
 server.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
