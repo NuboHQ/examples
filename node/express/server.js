3c01f5a9b3047dc9a0517ee43546ca11fa3d7ff4
Marcel Thomas <marcelthomas5@gmail.com>
2022-02-14 19:05:49 +0000

    Update server.js

--- node/express/server.js
+++ node/express/server.js
@@ -4,16 +4,22 @@ const PORT = process.env.PORT || 5555;
 const REGION = process.env.NUBO_REGION || 'unknown';
 const LOCATION = process.env.NUBO_LOCATION || 'unknown';
 
-const VERSION = '1';
+const VERSION = '1.0';
 
 server.use(helmet());
 
 server.use('*', (req, res) => {
   res.send(
     `<div style="font-family: sans-serif; text-align: center; position: fixed; top: 45%; left: 50%; transform: translateX(-50%) translateY(-50%);">
-            <h1>Express on Nubo</h1>
-            <p style="color: #777">${REGION}<br/><br/>${LOCATION}</p>
-         </div>`
+      <h1>Express on Nubo</h1>
+      <p style="color: #777">${VERSION}</p>
+      <br/>
+      <br/>
+      <p style="color: #777">${REGION}
+      <br/>
+      <br/>
+      ${LOCATION}</p>
+    </div>`
   );
 });
 
