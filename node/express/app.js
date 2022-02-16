a70baa56155e5e519e1ef8bf07b61fd105ec55b8
Marcel Thomas <marcelthomas5@gmail.com>
2022-02-16 16:35:07 +0000

    Updated express ui

---
+++ node/express/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const PORT = process.env.PORT || 5555;
+const RUNTIME = process.env.NUBO_RUNTIME || 'node';
+const REGION = process.env.NUBO_REGION || 'europe-west';
+const LOCATION = process.env.NUBO_LOCATION || 'london';
+const app = express();
+
+const dir = path.join(__dirname, './views');
+console.log(dir);
+
+app.use(express.static('public'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', dir);
+
+app.get('/', (req, res) => {
+  res.render('home', { runtime: RUNTIME, region: REGION, location: LOCATION });
+});
+
+app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
