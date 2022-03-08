eed3bc26bd7c223ba02976a73409a05279c41901
Marcel Thomas <marcelthomas5@gmail.com>
2022-03-08 13:25:44 +0000

    Update next.config.js

--- node/real-time/next.config.js
+++ node/real-time/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    apiKey: process.env.NUBO_API_KEY,
+    apiKey: process.env.NUBO_API_KEY || 'api',
+    publicApiKey: process.env.NEXT_PUBLIC_NUBO_API_KEY || 'public',
   },
 };
 
