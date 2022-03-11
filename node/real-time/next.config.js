35977d8ec76223048957c143ac7693726a68757f
Marcel Thomas <marcelthomas5@gmail.com>
2022-03-11 12:07:49 +0000

    Added users

--- node/real-time/next.config.js
+++ node/real-time/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    apiKey: process.env.NUBO_API_KEY || 'api',
-    publicApiKey: process.env.NEXT_PUBLIC_NUBO_API_KEY || 'public',
-  },
+  publicRuntimeConfig: {},
 };
 
 module.exports = nextConfig;
