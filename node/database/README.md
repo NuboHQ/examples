9818b3d328e10149783e0b8b3ad859d832eb5005
Marcel Thomas <marcelthomas5@gmail.com>
2022-12-17 10:52:00 +0000

    Added node database example

---
+++ node/database/README.md
@@ -0,0 +1,1 @@
+# Node deployed on Nubo
---
+++ node/database/src/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const PORT = Number(process.env.PORT) || 3000;
+const app = express();
+
+const prisma = new PrismaClient();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'node' });
+});
+
+app.get('/messages', async (req, res) => {
+  const messages = await prisma.message.findMany();
+
+  res.json(messages);
+});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
