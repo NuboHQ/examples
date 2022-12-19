06f0347b9af5cbb67daa6bbbb2f7552a1cc25218
Marcel Thomas <marcelthomas5@users.noreply.github.com>
2022-12-19 13:48:53 +0000

    Update main.ts

--- deno/main.ts
+++ deno/main.ts
@@ -1,11 +1,11 @@
 import { Client } from 'https://deno.land/x/mysql/mod.ts';
 
 const client = await new Client().connect({
-  hostname: 'aws-eu-west-2.connect.psdb.cloud',
-  port: 6700,
-  username: 'root',
-  password: 'zdbf7kA5xVSMhA367uWK',
-  db: 'railway',
+  hostname: '',
+  port: 0,
+  username: '',
+  password: '',
+  db: '',
 });
 
 const messages = await client.query(`select * from commands`);
